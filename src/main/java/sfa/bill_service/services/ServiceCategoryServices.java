cfd38fb1146b881893debaa0d15c729994425c27
Prince26122002 <prince.mittal.cse.2020@miet.ac.in>
2025-02-20 14:12:07 +0530

    make createCategoryInBulk

--- src/main/java/sfa/bill_service/services/ServiceCategoryServices.java
+++ src/main/java/sfa/bill_service/services/ServiceCategoryServices.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sfa.bill_service.constants.ApiErrorCodes;
 import sfa.bill_service.constants.Status;
+import sfa.bill_service.dto.req.ServicesReq;
 import sfa.bill_service.dto.res.ServiceCategoryRes;
+import sfa.bill_service.dto.res.ServicesRes;
 import sfa.bill_service.entities.ServiceCategory;
+import sfa.bill_service.entities.ServicesEntity;
 import sfa.bill_service.exceptions.NoSuchElementFoundException;
 import sfa.bill_service.exceptions.ValidationException;
 import sfa.bill_service.repositories.ServiceCategoryRepo;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +31,22 @@ public class ServiceCategoryServices {
         ServiceCategory serviceCategory = new ServiceCategory(name, Status.Active);
         serviceCategoryRepo.save(serviceCategory);
         return new ServiceCategoryRes(serviceCategory.getId(), serviceCategory.getName());
+    }
+
+    public List<ServiceCategoryRes> createCategoryInBulk(List<String> categoryNames) {
+        List<ServiceCategoryRes> serviceCategoryResList = new ArrayList<>();
+        for (String name : categoryNames) {
+            Optional<ServiceCategory> existingCategory = serviceCategoryRepo.findByName(name);
+            if (existingCategory.isPresent()) {
+                throw new ValidationException(ApiErrorCodes.CATEGORY_ALREADY_EXISTS.getErrorCode(),ApiErrorCodes.CATEGORY_ALREADY_EXISTS.getErrorMessage());
+            }
+
+            ServiceCategory serviceCategory = new ServiceCategory(name, Status.Active);
+            serviceCategoryRepo.save(serviceCategory);
+            serviceCategoryResList.add(new ServiceCategoryRes(serviceCategory.getId(), serviceCategory.getName()));
+        }
+
+        return serviceCategoryResList;
     }
 
     public ServiceCategoryRes getCategoryById(Long id){
